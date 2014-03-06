bc19b1aade4800faf4e90c1b9caee666fa3c8415
Jennifer Kessel <kessel.jenniferl@yahoo.com>
2014-03-06 18:28:13 +0000

    Limit length of Micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Feb25app::Application.routes.draw do
+  resources :microposts
+
+  root 'users#index'
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
